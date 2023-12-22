34a617b6c94d9f0a6a72a0e4069f1931543765ed
afidhehe <miftahullafid@gmail.com>
2023-12-22 17:23:12 +0700

    beres 12.22

--- qr_demo/qr_code.py
+++ qr_demo/qr_code.py
@@ -4,30 +4,30 @@ from io import BytesIO
 import qrcode
 
 
-def get_qr_code(data: str) -> str:
-	qr_code_bytes = get_qr_code_bytes(data, format="PNG")
-	base_64_string = bytes_to_base64_string(qr_code_bytes)
+def get_qr_code(data: str, box_size=3, border=4) -> str:
+    qr_code_bytes = get_qr_code_bytes(data, format="PNG", box_size=box_size, border=border)
+    base_64_string = bytes_to_base64_string(qr_code_bytes)
 
-	return add_file_info(base_64_string)
+    return add_file_info(base_64_string)
 
 
 def add_file_info(data: str) -> str:
-	"""Add info about the file type and encoding.
-	
-	This is required so the browser can make sense of the data."""
-	return f"data:image/png;base64, {data}"
+    """Add info about the file type and encoding.
+
+    This is required so the browser can make sense of the data."""
+    return f"data:image/png;base64, {data}"
 
 
-def get_qr_code_bytes(data, format: str) -> bytes:
-	"""Create a QR code and return the bytes."""
-	img = qrcode.make(data)
+def get_qr_code_bytes(data, format: str, box_size=3, border=4) -> bytes:
+    """Create a QR code and return the bytes."""
+    img = qrcode.make(data, box_size=box_size, border=border)
 
-	buffered = BytesIO()
-	img.save(buffered, format=format)
+    buffered = BytesIO()
+    img.save(buffered, format=format)
 
-	return buffered.getvalue()
+    return buffered.getvalue()
 
 
 def bytes_to_base64_string(data: bytes) -> str:
-	"""Convert bytes to a base64 encoded string."""
-	return b64encode(data).decode("utf-8")
+    """Convert bytes to a base64 encoded string."""
+    return b64encode(data).decode("utf-8")
