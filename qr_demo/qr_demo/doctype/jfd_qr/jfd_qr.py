ba7bb93cecdc2c028380dfbed8804d0b1e63bb89
afidhehe <miftahullafid@gmail.com>
2023-12-22 16:48:01 +0700

    trial keberapa

--- qr_demo/qr_demo/doctype/jfd_qr/jfd_qr.py
+++ qr_demo/qr_demo/doctype/jfd_qr/jfd_qr.py
@@ -4,26 +4,8 @@
 # import frappe
 
 from frappe.model.document import Document
+
 from qr_demo.qr_code import get_qr_code
-from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import padding
-import base64
-import os
-
-def encrypt(data, key,iv):
-		key = key[:16].encode('utf-8')
-		iv = iv[:16].encode('utf-8')
-		
-		cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
-		padder = padding.PKCS7(algorithms.AES.block_size).padder()
-
-		padded_data = padder.update(data.encode('utf-8')) + padder.finalize()
-
-		encryptor = cipher.encryptor()
-		encrypted_data = encryptor.update(padded_data) + encryptor.finalize()
-
-		return base64.b64encode(encrypted_data).decode('utf-8')
 
 class JFDQR(Document):
 	def after_save(self):
