3f7fee860a1163fee8a3c5146df875ba2de813ac
afidhehe <miftahullafid@gmail.com>
2023-12-24 02:27:16 +0700

    perbaikan 23.12

--- qr_demo/qr_demo/doctype/jfd_qr/jfd_qr.py
+++ qr_demo/qr_demo/doctype/jfd_qr/jfd_qr.py
@@ -10,11 +10,11 @@ from qr_demo.encrypt import encrypt_text
 
 class JFDQR(Document):
 	def validate(self):
-		konci = self.key
-		secret_key = konci.encode('utf-8')
+		secret_key = self.key
+		#secret_key = konci.encode('utf-8')
 		iv = bytes([0] * 16)  # 16 bytes IV
-		input_mentah = self.plain
-		input_text = input_mentah.encode('utf-8')
+		input_text = self.plain
+		#input_text = input_mentah.encode('utf-8')
 		encrypted_text = encrypt_text(input_text, secret_key, iv)
 		#generate and display QR
 		self.qr_code = get_qr_code(encrypted_text)
